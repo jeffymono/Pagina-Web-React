2e09338656fe78086a4edf7c5129d27c7a48970e
Cristian Narvaez <cris-narvaez@hotmail.com>
2020-06-07 15:05:38 -0500

    Implementacion del navbar (75%)

---
+++ src/components/body/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import '../css/body.css'
+
+class Body extends React.Component{
+    render(){
+        return(
+            <div>Body
+                
+            </div>
+            )
+    }
+}
+export default Body
--- src/components/pages/Principal.js
+++ src/components/pages/Principal.js
@@ -1,1 +1,20 @@
 import React from 'react'
+import Navbar from '../header/Navbar'
+import Body from '../body/Body'
+
+class Principal extends React.Component{
+    render(){
+        return(
+                <div>
+                    <Navbar 
+                        
+                    />
+                    <Body 
+
+                    />
+                </div>
+            
+        )
+    }
+}
+export default Principal
